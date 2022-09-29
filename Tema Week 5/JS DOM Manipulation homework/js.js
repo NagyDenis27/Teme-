dfba9fae51a75bd7d83973efb3859d9d2fa6ea1e
Nagy Denis <nagydenis27@gmail.com>
2022-09-30 01:37:38 +0200

    This is the init of the repository

--- Tema Week 5/JS DOM Manipulation homework/js.js
+++ Tema Week 5/JS DOM Manipulation homework/js.js
@@ -1,24 +1,16 @@
 function success() {
-  
-  var tag = document.createElement("div");
-
-  var text = document.createElement("div");
-
-  tag.appendChild(text) ;
-  
-
-  text.setAttribute('style','background-color:  rgb(193, 253, 133); color: darkgreen; width: auto; height:40px; border : 2px solid rgb(113, 192, 77); border-radius: 7px; display: flex; justify-content: flex-start;  align-items:center; margin-left: 30%; margin-right:30%; font-size:larger ; font-weight:550 ; font-style: arial; padding:4px;',);
-  
-
-  text.innerHTML ="Thank you for contacting us";
- 
 
   var parent = document.getElementById("pop-up");
+  var tag = document.createElement("div");
+  
+  parent.appendChild(tag);
 
+  tag.innerHTML = "Thank you for contacting us"
+  
   parent.setAttribute("style", "margin-top:20px;");
 
-  parent.appendChild(tag) ;
-
+  tag.setAttribute('style','background-color:  rgb(193, 253, 133); color: darkgreen; width: auto; height:40px; border : 2px solid rgb(113, 192, 77); border-radius: 7px; display: flex; justify-content: flex-start;  align-items:center; margin-left: 30%; margin-right:30%; font-size:larger ; font-weight:550 ; font-style: arial; padding:4px;',);
+  
   var input1 = document.getElementById("input-name").setAttribute("style","background-color: lightgreen; border-radius:5px;" )
   var input2 = document.getElementById("input-email").setAttribute("style","background-color: lightgreen;border-radius:5px; ")
   var input3 = document.getElementById("input-subject").setAttribute("style","background-color: lightgreen;border-radius:5px;")
@@ -30,26 +22,17 @@ function success() {
 
  function  error() {
 
-  var tag = document.createElement("div");
-
-  var text = document.createElement("div");
-
-  tag.appendChild(text) ;
-
-  
-
-  text.setAttribute('style','background-color: rgb(110, 11, 0); color: white; width: auto; height:40px; border : 2px solid rgb(113, 192, 77); border-radius: 7px; display: flex; justify-content: flex-start;  align-items:center; margin-left: 30%; margin-right:30%; font-size:larger ; font-weight:550 ; font-style: arial; padding:4px;',);
-  
-
-  text.innerHTML ="Please enter your data" ;
- 
-
   var parent = document.getElementById("pop-up");
-
-  parent.setAttribute("style", "margin-top:20px;");
+  var tag = document.createElement("div");
 
   parent.appendChild(tag) ;
 
+  tag.innerHTML ="Please enter your data" ;
+
+  parent.setAttribute("style", "margin-top:20px;");
+  
+  tag.setAttribute('style','background-color: rgb(110, 11, 0); color: white; width: auto; height:40px; border : 2px solid rgb(113, 192, 77); border-radius: 7px; display: flex; justify-content: flex-start;  align-items:center; margin-left: 30%; margin-right:30%; font-size:larger ; font-weight:550 ; font-style: arial; padding:4px;',);
+  
   var input1 = document.getElementById("input-name").setAttribute("style","background-color: red; border-radius:5px;" )
   var input2 = document.getElementById("input-email").setAttribute("style","background-color: red;border-radius:5px; ")
   var input3 = document.getElementById("input-subject").setAttribute("style","background-color: red;border-radius:5px;")
@@ -66,18 +49,12 @@ function success() {
     var z =document.forms["form1"]["text3"].value;
 
     if (x, y, z == "") {
-
-      
-      
+        
       document.getElementById("btn").onclick = function() {error()}; 
-
       return false;
-    }
-
-    else {
+    } else {
                    
       document.getElementById("btn").onclick = function() {success()}; 
-
       return;
 
     }
